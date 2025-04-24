3f876c410f7a9e28bd7ff95f16d2fd36bd99c976
itcast <hello@itcast.cn>
2025-04-24 16:01:42 +0800

    hello world

---
+++ src/main/java/com/itheima/Helloworld3Application.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Helloworld3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Helloworld3Application.class, args);
+    }
+
+}
